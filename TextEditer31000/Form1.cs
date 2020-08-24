e8c6c86b1466e5f5e1893fdcc4e254bc6f146926
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2020-08-24 10:57:36 +0900

    UI作成

--- TextEditer31000/Form1.cs
+++ TextEditer31000/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,43 @@ namespace TextEditer31000
         {
             InitializeComponent();
         }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //アプリケーション終了
+            Application.Exit();
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //［開く］ダイアログを表示
+            if (ofdFileOpen.ShowDialog() == DialogResult.OK)
+            {
+                //StreamReaderクラスを使用してファイル読込み
+                using (StreamReader sr = new StreamReader(ofdFileOpen.FileName, Encoding.GetEncoding("utf-8"),false))
+                {
+                    rtTextArea.Text = sr.ReadToEnd();
+                }
+            }
+        }
+
+        //上書き保存
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        //名前を付けて保存
+        private void SaveNameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //［名前を付けて保存］ダイアログを表示
+            if (sfdFileSave.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfdFileSave.FileName, false, Encoding.GetEncoding("utf-8")))
+                {
+                    sw.WriteLine(rtTextArea.Text);
+                }
+            }
+        }
+
     }
 }
